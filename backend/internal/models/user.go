08f2f558db9eb3b8c24c3cc927bf8b0385dea5bf
Enric Velasco Bufi <enric.velasco@csa.es>
2025-07-01 13:39:46 +0200

    fix: modified users model to use DTOs correctly

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -2,6 +2,10 @@ package models
 
 import "time"
 
+func (User) TableName() string {
+	return "Users"
+}
+
 type User struct {
 	ID            uint      `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name          string    `json:"name" gorm:"type:varchar(100);not null"`
@@ -17,19 +21,22 @@ type User struct {
 }
 
 type NonValidatedUser struct {
-	ID      uint      `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name    string    `json:"name" gorm:"type:varchar(100);not null"`
-	Surname string    `json:"surname" gorm:"type:varchar(100);not null"`
-	Email   string    `json:"email" gorm:"type:varchar(150);uniqueIndex;not null"`
-	Address string    `json:"address" gorm:"type:varchar(255)"`
-	CrtDate time.Time `json:"crt_date" gorm:"autoCreateTime"`
-	UptDate time.Time `json:"upt_date" gorm:"autoUpdateTime"`
+	ID            uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name          string    `json:"name" gorm:"type:varchar(100);not null"`
+	Surname       string    `json:"surname" gorm:"type:varchar(100);not null"`
+	Email         string    `json:"email" gorm:"type:varchar(150);uniqueIndex;not null"`
+	Address       string    `json:"address" gorm:"type:varchar(255)"`
+	TwoFactorAuth uint      `json:"two_factor_auth" gorm:"default:0"`
+	FailedLogins  uint      `json:"failed_logins" gorm:"default:0"`
+	IsBlocked     bool      `json:"is_blocked" gorm:"default:false"`
+	CrtDate       time.Time `json:"crt_date" gorm:"autoCreateTime"`
+	UptDate       time.Time `json:"upt_date" gorm:"autoUpdateTime"`
 }
 
 type SimplifiedUser struct {
-	ID      uint   `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Email   string `json:"email"`
-	Address string `json:"address"`
+	ID      uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name    string `json:"name" gorm:"type:varchar(100);not null"`
+	Surname string `json:"surname" gorm:"type:varchar(100);not null"`
+	Email   string `json:"email" gorm:"type:varchar(150);uniqueIndex;not null"`
+	Address string `json:"address" gorm:"type:varchar(255)"`
 }
