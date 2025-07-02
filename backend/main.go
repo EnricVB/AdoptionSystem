f8ff7f0d96dba67aa55cc36bfd47cd136e918e34
EnricVB <enricvbufi@gmail.com>
2025-07-02 14:46:23 +0200

    feat: modified constructor to allow headers

--- backend/main.go
+++ backend/main.go
@@ -35,6 +35,7 @@ func setupCORS() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:4200"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))
 
 	e.Start(":8080")
