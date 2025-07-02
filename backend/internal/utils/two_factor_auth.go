9f9efffd64f759c8edadc854b561828bc74e1af0
Enric Velasco Bufi <enric.velasco@csa.es>
2025-07-02 10:34:39 +0200

    feat: added 2fa code generator

---
+++ backend/internal/utils/two_factor_auth.go
@@ -0,0 +1,20 @@
+package utils
+
+import "crypto/rand"
+
+func Generate2FA(length int) string {
+	var characters = "0123456789"
+
+	code := make([]byte, length)
+	_, err := rand.Read(code)
+	if err != nil {
+		return ""
+	}
+
+	result := ""
+	for _, char := range code {
+		result += string(characters[char%byte(len(characters))])
+	}
+
+	return result
+}
