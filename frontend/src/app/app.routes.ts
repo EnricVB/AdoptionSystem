fa5a58f92c015fd42e884f29b071f553f3174006
Pablo Arranz <pablo.arranz@csa.es>
2025-07-03 12:45:22 +0200

    feat: login error message

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
+export const routes: Routes = [
+    {path: 'dashboard', component: Dashboard},
+    { path: '', component: Login },
+];
