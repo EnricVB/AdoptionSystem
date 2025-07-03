8b698018d907189f49dcd497a79c8f2ad5127928
EnricVB <enricvbufi@gmail.com>
2025-07-03 14:20:40 +0200

    feat: added Error response method to create a struct withou tthe echo.Context

--- backend/internal/utils/rest/error.go
+++ backend/internal/utils/rest/error.go
@@ -11,9 +11,27 @@ type HTTPError struct {
 ErrorResponse sends a JSON response with an error message and status code.
 It is used to standardize error responses in the API.
 */
-func ErrorResponse(c echo.Context, status int, message string) error {
-	return c.JSON(status, HTTPError{
-		Code:    status,
+func ErrorResponse(c echo.Context, code int, message string) error {
+	return c.JSON(code, HTTPError{
+		Code:    code,
 		Message: message,
+	})
+}
+
+/*
+Error creates an HTTPError instance with the given code and message.
+This is a utility function to create error responses.
+*/
+func Error(code int, message string) HTTPError {
+	return HTTPError{
+		Code:    code,
+		Message: message,
+	}
+}
+
+func ConvertToErrorResponse(c echo.Context, err HTTPError) error {
+	return c.JSON(err.Code, HTTPError{
+		Code:    err.Code,
+		Message: err.Message,
 	})
 }
