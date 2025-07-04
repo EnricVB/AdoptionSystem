bd1b1ce5a4fc947b403bfd1ac718376fbd8f9802
Pablo Arranz <pablo.arranz@csa.es>
2025-07-04 08:24:48 +0200

    feat: graphic interface 2fa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './pages/login/login';
+import { Twofa } from './pages/twofa/twofa';
 
 export const routes: Routes = [
     {path: '', component: Login },
+    {path:'twofa', component: Twofa},
     {path: 'dashboard', component: Dashboard},
 ];
