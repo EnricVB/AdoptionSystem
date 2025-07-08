2f72eee47f69c9bce6fad1b28e5467346a5fec7b
EnricVB <enricvbufi@gmail.com>
2025-07-08 13:40:39 +0200

    feat: Implement change password functionality with modal and form validation

--- backend/internal/services/backend_calls/user_services.go
+++ backend/internal/services/backend_calls/user_services.go
@@ -297,6 +297,12 @@ func ResetPassword(email string) (*string, error) {
 }
 
 func SendNewPassword(email string, password string) error {
+	// Set ChangePassword flag to true
+	err := dao.SetChangePasswordFlag(email, true)
+	if err != nil {
+		return fmt.Errorf("error al establecer la bandera de cambio de contraseña: %v", err)
+	}
+
 	// Send new password via email
 	mailerErr := mailer.SendPassword(email, password)
 	if mailerErr != nil {
