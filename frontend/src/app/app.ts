5eda1856bb9f7d43ff44adf7420e7964e3b8a568
EnricVB <enricvbufi@gmail.com>
2025-07-09 08:49:00 +0200

    feat: Update application title and remove unused components and methods

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html'
+  template: `<router-outlet></router-outlet>`,
 })
 export class App {
-  protected title = 'frontend';
+  protected title = 'Adoption Sys';
 }
