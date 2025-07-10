bc4f3cbd12648b331a65bb90db5fc62fd5249ce7
EnricVB <enricvbufi@gmail.com>
2025-07-10 13:19:28 +0200

    feat: added svg wave component, modified dashboard to add banner image, and created call-to-action component

---
+++ frontend/src/app/components/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Wave } from "@app/components/wave/wave";
+
+@Component({
+  selector: 'Footer',
+  imports: [Wave],
+  templateUrl: './footer.html',
+})
+export class Footer {
+
+
+}
