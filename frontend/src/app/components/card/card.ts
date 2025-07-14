30885ca7d70d6f4ef1528ebb0ce9f6214b263b3f
Enric Velasco Bufi <enric.velasco@csa.es>
2025-07-14 14:21:09 +0200

    feat: Enhance pet card display with age and vaccination status, and improve layout consistency

--- frontend/src/app/components/card/card.ts
+++ frontend/src/app/components/card/card.ts
@@ -11,8 +11,10 @@ export class Card {
   @Input() title!: string;
   @Input() species!: string;
   @Input() image!: string;
-  @Input() isAdopted!: boolean;
+  @Input() status: string = "Available";
   @Input() description!: string; 
+  @Input() age!: string;
+  @Input() vaccinated!: string;
 
   constructor() {
   } 
