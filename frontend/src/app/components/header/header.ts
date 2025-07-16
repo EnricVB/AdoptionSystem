82c8801cd20bdf26957f7ca82a1f4f19b50aba2d
EnricVB <enricvbufi@gmail.com>
2025-07-16 13:22:06 +0200

    feat: Enhance routing and navigation in header component for better user experience

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CookieService } from '@app/services/cookie.service';
 
 @Component({
   selector: 'PetHeader',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
 })
 export class PetHeader {
