49d957fbd045055ec84210e4f42386f3a4fa84bf
EnricVB <enricvbufi@gmail.com>
2025-07-16 13:17:13 +0200

    feat: Update header component to use Angular routerLink for navigation

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CookieService } from '@app/services/cookie.service';
 
 @Component({
   selector: 'PetHeader',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
 })
 export class PetHeader {
